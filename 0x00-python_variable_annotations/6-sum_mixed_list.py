a09fcddacefdde830d8bb5df99c9f6466b0409f6
ahmednabil14 <ahmednabil14499@gmail.com>
2024-06-17 23:21:41 +0300

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@ from typing import List, Union
 """task 6"""
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum mixed list"""
     sum: float = 0
     for li in mxd_lst:
