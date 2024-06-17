fc04c770a8d16c664d407f6b6cd7aac7235b5316
ahmednabil14 <ahmednabil14499@gmail.com>
2024-06-17 23:12:07 +0300

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from typing import List
 """task 5"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     sum: float = 0
     for li in input_list:
         sum += li
